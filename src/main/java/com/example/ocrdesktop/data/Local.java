fa8814a245d4b3bf172fba1bbc52abbf8985d455
Hedrax <88040920+Hedrax@users.noreply.github.com>
2024-12-17 14:28:51 +0200

    Adding draft local, remote classes

---
+++ src/main/java/com/example/ocrdesktop/data/Local.java
@@ -0,0 +1,8 @@
+package com.example.ocrdesktop.data;
+
+
+
+
+//TODO handle or caching and local storage interactions
+public class Local {
+}
---
+++ src/main/java/com/example/ocrdesktop/data/Remote.java
@@ -0,0 +1,8 @@
+package com.example.ocrdesktop.data;
+
+
+
+
+//TODO handle all service and storage interactions with the backend agent
+public class Remote {
+}
