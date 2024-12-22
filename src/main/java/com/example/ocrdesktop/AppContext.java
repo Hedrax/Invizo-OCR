24256d370db1aeb2ec9dc02255a68f3e64d1ec65
Hedrax <88040920+Hedrax@users.noreply.github.com>
2024-12-23 00:53:27 +0200

    adding AppContext Class

---
+++ src/main/java/com/example/ocrdesktop/AppContext.java
@@ -0,0 +1,40 @@
+package com.example.ocrdesktop;
+
+import javafx.beans.property.ReadOnlyObjectWrapper;
+
+
+//Note: that you can append variables to the class based on the required,
+// but never modify without notifying the rest of the team
+public class AppContext {
+
+    // Private static instance of the class
+    private static AppContext instance;
+
+    // Properties to store globally accessible objects
+    private final ReadOnlyObjectWrapper<Double> stageWidth = new ReadOnlyObjectWrapper<>(0.0);
+    private final ReadOnlyObjectWrapper<Double> stageHeight = new ReadOnlyObjectWrapper<>(0.0);
+    // Public method to get the single instance of the class
+    public static synchronized AppContext getInstance() {
+        if (instance == null) {
+            instance = new AppContext();
+        }
+        return instance;
+    }
+
+    public void setStageWidth(Double value){
+        this.stageWidth.set(value);
+    }
+    public double getStageWidth() {
+            return stageWidth.get();
+    }
+    public ReadOnlyObjectWrapper<Double> getWidthReadProperty(){return stageWidth;}
+
+    // Example: Add more properties as needed
+    public void setWeight(double newWidth) {
+        this.stageWidth.set(newWidth);
+    }
+
+    public void setHeight(Double newWidth) {
+        this.stageHeight.set(newWidth);
+    }
+}
