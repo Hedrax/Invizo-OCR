7e4e2c904c57e959c0a243ad024f9dfa76f704c7
Hedrax <88040920+Hedrax@users.noreply.github.com>
2024-12-23 17:35:22 +0200

    Adding Confirm Status

--- src/main/java/com/example/ocrdesktop/utils/Item.java
+++ src/main/java/com/example/ocrdesktop/utils/Item.java
@@ -10,10 +10,20 @@ public class Item {
     public ObservableList<String> values;
     public String image_path;
     public String date;
+    public boolean confirmed = false;
     public Item(Sheet sheet, ObservableList<String> values, String image_path, String date){
         this.image_path = image_path;
         this.values = values;
         this.sheet = sheet;
         this.date = date;
+    }
+
+    //Todo merge once done
+    public Item(Sheet sheet, ObservableList<String> values, String image_path, String date, boolean confirmed){
+        this.image_path = image_path;
+        this.values = values;
+        this.sheet = sheet;
+        this.date = date;
+        this.confirmed = confirmed;
     }
 }
