6ecb0d92d764cce54540d48798ef11600293945b
Hedrax <88040920+Hedrax@users.noreply.github.com>
2024-12-23 17:36:08 +0200

    adding ID attribute

--- src/main/java/com/example/ocrdesktop/utils/Receipt.java
+++ src/main/java/com/example/ocrdesktop/utils/Receipt.java
@@ -5,8 +5,13 @@ package com.example.ocrdesktop.utils;
 //Note: that you can append variables to the class based on the required,
 // but never modify without notifying the rest of the team
 public class Receipt {
+    public String id;
     public String name;
     public Receipt(String name){
         this.name = name;
+    }
+    public Receipt(String id, String name){
+        this.name = name;
+        this.id = id;
     }
 }
