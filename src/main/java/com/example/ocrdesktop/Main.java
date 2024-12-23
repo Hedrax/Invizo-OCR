df46d9d2816f74af49531620bd03835e0399b7ad
Hedrax <88040920+Hedrax@users.noreply.github.com>
2024-12-23 17:35:01 +0200

    initializing AppContext

--- src/main/java/com/example/ocrdesktop/Main.java
+++ src/main/java/com/example/ocrdesktop/Main.java
@@ -8,13 +8,30 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/ocrdesktop/main_layout.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/ocrdesktop/detail_items.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
         stage.setTitle("Invizo");
         stage.setScene(scene);
         stage.show();
+
+        AppContext.getInstance().setWeight(stage.getWidth());
+        AppContext.getInstance().setHeight(stage.getHeight());
+
+        //Todo make the following callbacks on sub thread to avoid affecting io performance
+        //width callback
+        stage.widthProperty().addListener((obj, oldWidth, newWidth)->{
+            //callBack Context
+            AppContext.getInstance().setStageWidth((Double) newWidth);
+        });
+        //width callback
+        stage.heightProperty().addListener((obj, oldWidth, newWidth)->{
+            //callBack Context
+            AppContext.getInstance().setHeight((Double) newWidth);
+        });
+
     }
 // ali
     public static void main(String[] args) {
