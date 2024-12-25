99aa94f3a359cb6cb9695a050d75c5e4c1b88403
Ali ibrahim Ahmed <116113529+AliFeteha@users.noreply.github.com>
2024-12-25 21:53:01 +0200

    add sqlite dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.example.ocrdesktop {
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
 
     opens com.example.ocrdesktop to javafx.fxml;
