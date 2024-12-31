13e806ffe52209c9321d25e24841174074a372ac
Ali ibrahim Ahmed <116113529+AliFeteha@users.noreply.github.com>
2024-12-31 02:31:51 +0200

    edit request structure

--- src/main/java/com/example/ocrdesktop/utils/Request.java
+++ src/main/java/com/example/ocrdesktop/utils/Request.java
@@ -2,6 +2,8 @@ package com.example.ocrdesktop.utils;
 
 import javafx.collections.ObservableList;
 
+import java.sql.Time;
+
 //Note: that you can append variables to the class based on the required,
 // but never modify without notifying the rest of the team
 public class Request {
@@ -9,10 +11,19 @@ public class Request {
     public ObservableList<Item> items;
     public ReceiptType receiptType;
     public String date;
+    public String status;
+    public String uploaded_by_user_id;
+    public Time uploaded_at;
     public Request(String id, ObservableList<Item> items, ReceiptType receiptType, String date){
         this.items = items;
         this.receiptType = receiptType;
         this.id = id;
         this.date = date;
+    }
+    public Request(String id, String status, String uploaded_by_user_id, Time uploaded_at){
+        this.id = id;
+        this.status = status;
+        this.uploaded_by_user_id = uploaded_by_user_id;
+        this.uploaded_at = uploaded_at;
     }
 }
