cd5d642676d9bb7dc72607a4413c1869f87b498c
Ali ibrahim Ahmed <116113529+AliFeteha@users.noreply.github.com>
2024-12-31 02:31:30 +0200

    add some database functions and link in repo with remote

--- src/main/java/com/example/ocrdesktop/data/Remote.java
+++ src/main/java/com/example/ocrdesktop/data/Remote.java
@@ -1,8 +1,19 @@
 package com.example.ocrdesktop.data;
 
 
+import com.example.ocrdesktop.utils.Receipt;
+import com.example.ocrdesktop.utils.ReceiptType;
 import com.example.ocrdesktop.utils.ReceiptTypeJSON;
+import com.example.ocrdesktop.utils.Request;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.json.JSONObject;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import static com.example.ocrdesktop.data.Local.getAllReceiptTypeNames;
 
 //TODO handle all service and storage interactions with the backend agent
 public class Remote {
@@ -15,5 +26,21 @@ public class Remote {
 
 
         return 200;
+    }
+    public static ObservableList<ReceiptType> getReceiptTypes() {
+        ObservableList<ReceiptType> receiptTypes = FXCollections.observableArrayList();
+        //TODO Rewan need to get all receiptTypes from receipt_type table in  receiptType format
+        return receiptTypes;
+    }
+    public static ObservableList<Request> getRequests() {
+        ObservableList<Request> Requests = FXCollections.observableArrayList();
+        //TODO Rewan need to get all Requests from upload_requests table in  Request format
+        return Requests;
+    }
+    public static ObservableList<Receipt> getReceipts() {
+        ObservableList<Receipt> Receipts = FXCollections.observableArrayList();
+        //TODO Rewan need to get all Receipts from receipt table in  Receipt format
+        // need to convert json to map<string,string>
+        return Receipts;
     }
 }
--- src/main/java/com/example/ocrdesktop/data/Repo.java
+++ src/main/java/com/example/ocrdesktop/data/Repo.java
@@ -1,10 +1,21 @@
 package com.example.ocrdesktop.data;
 
+import com.example.ocrdesktop.utils.Receipt;
 import com.example.ocrdesktop.utils.ReceiptType;
 import com.example.ocrdesktop.utils.ReceiptTypeJSON;
+import com.example.ocrdesktop.utils.Request;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.*;
+import java.util.*;
+
+import static com.example.ocrdesktop.data.Local.*;
+import static com.example.ocrdesktop.data.Remote.*;
 
 public class Repo {
     Remote remote = new Remote();
+
     public boolean checkReceiptTypeNameAvailable(String text) {
         //TODO check all name of ReceiptType in the local database and return boolean True: name Available, False: name is reserved by another object
         return true;
@@ -19,10 +30,8 @@ public class Repo {
 
         if (response == 200) {
             //TODO save receipt Type in the local database on response
-
         }
         return response;
-
     }
 
     public int modifyReceiptType(ReceiptTypeJSON receiptTypeJSON, String oldName) {
@@ -36,42 +45,48 @@ public class Repo {
         //remote.modifyReceiptType(receiptTypeJSON);
         return 200;
     }
-  /*  // Database connection helper
+
+    // Database connection helper
     private static Connection getDatabaseConnection() throws SQLException {
         String url = "jdbc:sqlite:receipts.db";
         return DriverManager.getConnection(url);
     }
 
     // Dummy data for ReceiptType
-    private static List<ReceiptType> getDummyReceiptTypes() {
-        List<ReceiptType> receiptTypes = new ArrayList<>();
-        receiptTypes.add(new ReceiptType("1", "Invoice", "An invoice receipt"));
-        receiptTypes.add(new ReceiptType("2", "Payment", "A payment receipt"));
+    private static ObservableList<ReceiptType> getDummyReceiptTypes() {
+        ObservableList<ReceiptType> receiptTypes = FXCollections.observableArrayList();
+        receiptTypes.add(new ReceiptType("Invoice", Arrays.asList("field1", "field2")));
+        receiptTypes.add(new ReceiptType("Payment", Arrays.asList("field3", "field4")));
         return receiptTypes;
     }
 
-    // Dummy data for ReceiptTypeFields
-    private static List<ReceiptTypeFields> getDummyReceiptTypeFields() {
-        List<ReceiptTypeFields> receiptTypeFields = new ArrayList<>();
-        receiptTypeFields.add(new ReceiptTypeFields("1", "Field1", "String"));
-        receiptTypeFields.add(new ReceiptTypeFields("1", "Field2", "Integer"));
-        receiptTypeFields.add(new ReceiptTypeFields("2", "Field3", "Date"));
-        return receiptTypeFields;
-    }
-
     // Dummy data for Request
-    private static List<Request> getDummyRequests() {
-        List<Request> requests = new ArrayList<>();
-        requests.add(new Request("1", "Pending", "user1", String.valueOf(new Timestamp(System.currentTimeMillis()))));
-        requests.add(new Request("2", "Completed", "user2", String.valueOf(new Timestamp(System.currentTimeMillis()))));
+    private static ObservableList<Request> getDummyRequests() {
+        ObservableList<Request> requests = FXCollections.observableArrayList();
+        requests.add(new Request("1", "Pending", "user1", new Time(System.currentTimeMillis())));
+        requests.add(new Request("2", "Completed", "user2", new Time(System.currentTimeMillis())));
         return requests;
     }
 
-    // Dummy data for Receipt
-    private static List<Receipt> getDummyReceipts() {
-        List<Receipt> receipts = new ArrayList<>();
-        receipts.add(new Receipt("1", "1", "1", "1", "image1.png", "pending", "152",String.valueOf(new Timestamp(System.currentTimeMillis()))));
-        receipts.add(new Receipt("1", "1", "1", "1", "image1.png", "pending", "152",String.valueOf(new Timestamp(System.currentTimeMillis()))));
+    private static ObservableList<Receipt> getDummyReceipts() {
+        ObservableList<Receipt> receipts = FXCollections.observableArrayList();
+
+        // Creating dummy OCR data
+        Map<String, String> ocrData1 = new HashMap<>();
+        ocrData1.put("Field1", "Value1");
+        ocrData1.put("Field2", "Value2");
+
+        Map<String, String> ocrData2 = new HashMap<>();
+        ocrData2.put("Field3", "Value3");
+        ocrData2.put("Field4", "Value4");
+
+        // Format the current timestamp as a String
+        String currentTimestamp = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
+
+        // Adding receipts with dummy data
+        receipts.add(new Receipt("1", "Invoice", "1", "image1.png", "Pending", ocrData1, "user152", currentTimestamp));
+        receipts.add(new Receipt("2", "Payment", "2", "image2.png", "Approved", ocrData2, "user153", currentTimestamp));
+
         return receipts;
     }
 
@@ -79,46 +94,29 @@ public class Repo {
     public static void refreshData() {
         try (Connection localConnection = getDatabaseConnection()) {
             // Get dummy data
-            List<ReceiptType> receiptTypes = getDummyReceiptTypes();
-            List<ReceiptTypeFields> receiptTypeFields = getDummyReceiptTypeFields();
-            List<Request> requests = getDummyRequests();
-            List<Receipt> receipts = getDummyReceipts();
+            ObservableList<ReceiptType> receiptTypes = getDummyReceiptTypes();
+            ObservableList<Request> requests = getDummyRequests();
+            ObservableList<Receipt> receipts = getDummyReceipts();
+           /* ObservableList<ReceiptType> receiptTypes = getReceiptTypes();
+            ObservableList<Request> requests = getRequests();
+            ObservableList<Receipt> receipts = getReceipts();*/
             refreshReceiptType(localConnection, receiptTypes);
             refreshUploadRequests(localConnection, requests);
             refreshReceipt(localConnection, receipts);
-            createTablesForReceiptTypes(localConnection);
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void getAllReceipts() {
+    public static ObservableList<String> getReceiptTypeNames() {
+        ObservableList<String> receiptTypeNames = FXCollections.observableArrayList();
         try (Connection localConnection = getDatabaseConnection()) {
-            // Retrieve and print all tables and their columns
-            DatabaseMetaData metaData = localConnection.getMetaData();
-
-            // Get the list of tables (assuming you're using SQLite or similar databases)
-            try (ResultSet tablesResultSet = metaData.getTables(null, null, "%", new String[]{"TABLE"})) {
-                while (tablesResultSet.next()) {
-                    String tableName = tablesResultSet.getString("TABLE_NAME");
-                    System.out.println("Table: " + tableName);
-
-                    // Get the columns for the current table
-                    try (ResultSet columnsResultSet = metaData.getColumns(null, null, tableName, "%")) {
-                        while (columnsResultSet.next()) {
-                            String columnName = columnsResultSet.getString("COLUMN_NAME");
-                            String columnType = columnsResultSet.getString("TYPE_NAME");
-                            System.out.println("\tColumn: " + columnName + " | Type: " + columnType);
-                        }
-                    }
-                }
-            }
+            // Assuming getAllReceiptTypeNames() returns a List<String>
+            List<String> receiptTables = getAllReceiptTypeNames(localConnection);
+            receiptTypeNames.addAll(receiptTables); // Adding items to ObservableList
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return receiptTypeNames;
     }
-
-*/
-
 }
