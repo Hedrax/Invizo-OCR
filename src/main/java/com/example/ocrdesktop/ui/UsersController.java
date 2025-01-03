45c8a29e38a215bb5c808670693c5db617417fd8
Hedrax <88040920+Hedrax@users.noreply.github.com>
2025-01-03 16:19:16 +0200

    adding UsersController.java and all depending classes

---
+++ src/main/java/com/example/ocrdesktop/ui/UsersController.java
@@ -0,0 +1,181 @@
+package com.example.ocrdesktop.ui;
+
+import com.example.ocrdesktop.AppContext;
+import com.example.ocrdesktop.control.NavigationManager;
+import com.example.ocrdesktop.data.Repo;
+import com.example.ocrdesktop.ui.subelements.UserControlEntryController;
+import com.example.ocrdesktop.utils.User;
+import javafx.animation.TranslateTransition;
+import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.util.List;
+
+public class UsersController {
+
+    public AnchorPane mainContent;
+    public AnchorPane sideMenu;
+    @FXML
+    public VBox userListVbox ;
+    public Label confirmUpdatesButton;
+    public Label profileNameTopBanner;
+    public Label profileCompanyTopBanner;
+    public ImageView profilePictureSideMenuLabel;
+    public Label profileNameSideMenuLabel;
+    public Label profileRoleSideMenuLabel;
+    private boolean isMenuVisible = false; // Tracks menu state
+    private final Repo repo = new Repo();
+    ObservableList<User> lst = FXCollections.observableArrayList();
+    List<User> deletedUsers = FXCollections.observableArrayList();
+    BooleanProperty edited = new SimpleBooleanProperty(false);
+
+    @FXML
+    private void toggleMenu() {
+
+        TranslateTransition transition = new TranslateTransition(Duration.millis(300), sideMenu);
+
+        if (isMenuVisible) {
+            // Slide out (hide)
+            this.mainContent.setDisable(false);
+            transition.setToX(-300); // Hide the menu
+            mainContent.setEffect(null); // Apply blur
+        } else {
+            // Slide in (show)
+            this.mainContent.setDisable(true);
+            transition.setToX(0); // Show the menu
+            mainContent.setEffect(new GaussianBlur(10)); // Apply blur
+        }
+
+        transition.play();
+        isMenuVisible = !isMenuVisible; // Toggle the menu state
+    }
+    void initFakeData() {
+        lst.add(new User("admin", "admin123", "admin.admin@admin.com", User.Role.SUPER_ADMIN));
+        lst.add(new User("Dummy_id2", "Dummy_name2", "Dummy_email2", User.Role.DESKTOP_USER));
+        lst.add(new User("Dummy_id3", "Dummy_name3", "Dummy_email3", User.Role.MOBILE_USER));
+        lst.add(new User("Dummy_id1", "Dummy_name1", "Dummy_email1", User.Role.SUPER_ADMIN));
+        lst.add(new User("Dummy_id2", "Dummy_name2", "Dummy_email2", User.Role.DESKTOP_USER));
+        lst.add(new User("Dummy_id3", "Dummy_name3", "Dummy_email3", User.Role.MOBILE_USER));
+        lst.add(new User("Dummy_id1", "Dummy_name1", "Dummy_email1", User.Role.SUPER_ADMIN));
+        lst.add(new User("Dummy_id2", "Dummy_name2", "Dummy_email2", User.Role.DESKTOP_USER));
+    }
+
+    void getDataFromRepo(){
+        lst.addAll(repo.getUsers());
+    }
+
+    private void deleteUser(User user, HBox pane){
+        lst.remove(user);
+        deletedUsers.add(user);
+        userListVbox.getChildren().remove(pane);
+    }
+
+    private HBox loadEntry(User user) {
+        try {
+        // Load the custom view FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/ocrdesktop/user_control_entry.fxml"));
+        HBox pane = loader.load();
+
+        // Get the controller and set the data
+        UserControlEntryController controller = loader.getController();
+        controller.setData(user);
+
+        controller.deleteProperty.addListener((obs, old, val) -> {
+            if (val) {
+                deleteUser(user, pane);
+            }
+        });
+        controller.editProperty.addListener((obs, old, val) -> {
+            if (val) {
+                edited.set(true);
+            }
+        });
+        return pane;
+        }catch (IOException ignore){}
+        return null;
+    }
+    @FXML
+    private void initialize() {
+        edited.addListener((obs, old, val) -> {
+            confirmUpdatesButton.setDisable(!val);
+        });
+
+        // Add a listener to synchronize changes from the ObservableList to the VBox
+        lst.addListener((ListChangeListener<User>) change -> {
+            while (change.next()) {
+                if (change.wasAdded()) {
+                    for (User addedItem : change.getAddedSubList()) {
+                        HBox pane = loadEntry(addedItem); // Create a new label for each added item
+                        userListVbox.getChildren().add(pane);      // Add the label to the VBox
+                    }
+                }
+            }
+        });
+        getDataFromRepo();
+    }
+
+    public UsersController() {
+        initialize();
+        setUpProfileInfo();
+    }
+
+    @FXML
+    private void setUpProfileInfo(){
+        String userName = AppContext.getInstance().getAuthorizationInfo().currentUser.userName;
+        String organizationName = AppContext.getInstance().getAuthorizationInfo().organization.name;
+        String role = AppContext.getInstance().getAuthorizationInfo().currentUser.role.toString().replace("_", " ");
+        Platform.runLater(() -> {
+            profileNameTopBanner.setText(userName);
+            profileCompanyTopBanner.setText(organizationName);
+            profileNameSideMenuLabel.setText(userName);
+            profileRoleSideMenuLabel.setText(role);
+        });
+    }
+
+    @FXML
+    private void navigateToAllRequests(){
+        NavigationManager.getInstance().navigateToRequestsPage();}
+    @FXML
+    private void navigateToSheets(){
+        NavigationManager.getInstance().navigateToSHOWCSVs();}
+    @FXML
+    private void navigateToUsersManger(){}
+    @FXML
+    private void navigateToProfile(){}
+    @FXML
+    private void navigateToSettings(){}
+    @FXML
+    private void Logout(){try{NavigationManager.getInstance().logout();}catch (Exception e){e.printStackTrace();}}
+
+    public void navigateToMain() { NavigationManager.getInstance().navigateToMainPage(); }
+
+    @FXML
+    private void confirmUpdates(){
+        //make a callback to the repo with the updates
+        List<User> oldUsers = repo.getUsers();
+        for (User user : lst) {
+            if (!oldUsers.contains(user)) {
+                repo.addUser(user);
+            } else{
+                repo.updateUser(user);
+            }
+        }
+        repo.deleteUsers(deletedUsers);
+    }
+}
+
---
+++ src/main/java/com/example/ocrdesktop/ui/subelements/UserControlEntryController.java
@@ -0,0 +1,111 @@
+package com.example.ocrdesktop.ui.subelements;
+
+import com.example.ocrdesktop.AppContext;
+import com.example.ocrdesktop.utils.User;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class UserControlEntryController {
+    public User user;
+    public Label userLabel;
+    public TextField emailTextField;
+    public TextField passwordTextField;
+    public ChoiceBox choiceBox;
+    public StackPane deleteButton;
+    HashMap<User.Role, String> role2ChoiceMap = new HashMap<>();
+    public BooleanProperty deleteProperty = new SimpleBooleanProperty(false);
+    public BooleanProperty editProperty = new SimpleBooleanProperty(false);
+
+    public UserControlEntryController() {
+        role2ChoiceMap.put(User.Role.SUPER_ADMIN, "Super Admin");
+        role2ChoiceMap.put(User.Role.DESKTOP_USER, "Desktop User");
+        role2ChoiceMap.put(User.Role.MOBILE_USER, "Mobile User");
+    }
+
+    private void showConfirmationDialog() {
+        AtomicBoolean approved = new AtomicBoolean(false);
+
+        String title = "Confirmation Dialog";
+        Alert dialog = new Alert(Alert.AlertType.INFORMATION);
+        dialog.setTitle(title);
+        dialog.setHeaderText(null);
+        dialog.setContentText("Are you sure you want to delete that user?");
+
+        // Add custom buttons
+        ButtonType okButtonType = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+        ButtonType cancelButtonType = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+        dialog.getButtonTypes().clear();
+        dialog.getDialogPane().getButtonTypes().addAll(okButtonType, cancelButtonType);
+
+        // Handle dialog closure
+        dialog.setOnCloseRequest(event -> {
+            if (approved.get()) deleteProperty.set(true);
+        });
+
+        // Add validation and result setting
+        Button okButton = (Button) dialog.getDialogPane().lookupButton(okButtonType);
+
+
+        okButton.setFocusTraversable(false);
+
+        okButton.addEventFilter(ActionEvent.ACTION, event -> {approved.set(true);});
+        // Show dialog and handle the result
+        dialog.showAndWait();
+    }
+
+    public void deleteUser(){
+        showConfirmationDialog();
+    }
+
+    public void setData(User user) {
+        this.user = user;
+        userLabel.setText(user.userName);
+        emailTextField.setText(user.email);
+        passwordTextField.setText("********");
+        choiceBox.setValue(role2ChoiceMap.get(user.role));
+        if (user.role == User.Role.SUPER_ADMIN && AppContext.getInstance().getAuthorizationInfo().currentUser.role != User.Role.SUPER_ADMIN) {
+            disableEdit();
+        }
+        if (user.id.equals(AppContext.getInstance().getAuthorizationInfo().currentUser.id)) {
+            disableDelete();
+        }
+
+        setEditListener();
+
+    }
+    private void setEditListener(){
+        userLabel.textProperty().addListener((obs) -> {
+            editProperty.set(true);
+        });
+        emailTextField.textProperty().addListener((obs) -> {
+            editProperty.set(true);
+        });
+        passwordTextField.textProperty().addListener((obs) -> {
+            editProperty.set(true);
+        });
+        choiceBox.valueProperty().addListener((obs) -> {
+            editProperty.set(true);
+        });
+        deleteProperty.addListener((obs) -> {
+            editProperty.set(true);
+        });
+    }
+
+    private void disableEdit(){
+        emailTextField.setDisable(true);
+        emailTextField.setText("*************@*********");
+        passwordTextField.setDisable(true);
+        choiceBox.setDisable(true);
+        disableDelete();
+    }
+    private void disableDelete() {
+        deleteButton.setDisable(true);
+    }
+}
