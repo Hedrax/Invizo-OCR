317ea6c9629fbbcce48132ad28f968d12f5da15b
Hedrax <88040920+Hedrax@users.noreply.github.com>
2025-01-03 16:17:44 +0200

    Adding a fake credentials on startup

--- src/main/java/com/example/ocrdesktop/Main.java
+++ src/main/java/com/example/ocrdesktop/Main.java
@@ -2,6 +2,9 @@ package com.example.ocrdesktop;
 
 import com.example.ocrdesktop.control.NavigationManager;
 import com.example.ocrdesktop.data.DatabaseInitializer;
+import com.example.ocrdesktop.utils.AuthorizationInfo;
+import com.example.ocrdesktop.utils.Organization;
+import com.example.ocrdesktop.utils.User;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -11,7 +14,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        //Setup fake user credentials for testing
+        initFakeData();
         NavigationManager.getInstance().start(stage);
+    }
+
+    private void initFakeData(){
+        User user = new User("admin", "admin123", "admin.admin@admin.com", User.Role.SUPER_ADMIN);
+        Organization organization = new Organization("Dummy", "Dummy");
+        AppContext.getInstance().setAuthorizationInfo(new AuthorizationInfo(user, organization));
     }
 
     public static void main(String[] args) {
