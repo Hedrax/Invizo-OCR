eb73094ab7f5063dc4ac9b8120df89de7daac14b
ryabubaker <63212411+ryabubaker@users.noreply.github.com>
2025-01-15 00:25:04 +0200

    added aws api

--- src/main/java/com/example/ocrdesktop/utils/ApiClient.java
+++ src/main/java/com/example/ocrdesktop/utils/ApiClient.java
@@ -19,7 +19,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ApiClient {
 
     private static final ApiClient INSTANCE = new ApiClient();
-    private static final String BASE_URL = "https://invizo-app.koyeb.app"; // Adjust as needed
+    private static final String BASE_URL = "http://invizo-app-env.eba-3rc9msxb.us-east-2.elasticbeanstalk.com"; // Adjust as needed
     private static final int MAX_RETRIES = 3;
     private static final Duration RETRY_DELAY = Duration.ofSeconds(2);
 
