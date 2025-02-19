b74c8da83f6cbd504e64f8f41f04e6270094a864
Hedrax <88040920+Hedrax@users.noreply.github.com>
2025-02-20 00:34:19 +0200

    making sure that ocrData is initialized since were creating it locally in the portable version

--- src/main/java/com/example/ocrdesktop/utils/Receipt.java
+++ src/main/java/com/example/ocrdesktop/utils/Receipt.java
@@ -13,11 +13,11 @@ public class Receipt {
     public String requestId;
     public String imageUrl;
     public ReceiptStatus status; // Can be converted to an Enum if needed
-    public HashMap<Integer, String> ocrData;
+    public HashMap<Integer, String> ocrData = new HashMap<>();
     public String approvedByUserId;
     public Timestamp approvedAt; // Use String for simplicity, convert to Date/Timestamp as needed
     public Path imagePath;
-// note that there are two Receipt constructor
+    // note that there are two Receipt constructor
     // these constructor for ui
     public Receipt(String receiptId, String receiptTypeId, String requestId, String imageUrl, String status, HashMap<Integer, String> ocrData, String approvedByUserId, Timestamp approvedAt) {
         this.receiptId = receiptId;
@@ -25,7 +25,9 @@ public class Receipt {
         this.requestId = requestId;
         this.imageUrl = imageUrl;
         this.status = ReceiptStatus.valueOf(status);
-        this.ocrData = ocrData;
+        if (ocrData != null) {
+            this.ocrData = ocrData;
+        }
         this.approvedByUserId = approvedByUserId;
         this.approvedAt = approvedAt;
     }
