754abb1a0b4908a3665354187e20ff5cb6ab8def
Hedrax <88040920+Hedrax@users.noreply.github.com>
2025-02-26 21:22:25 +0200

    feat: Add new Receipt constructor and default values
    Added a new constructor to the Receipt class to
    handle cases where a new receipt is created
    locally.  Also added default values for imageURL
    and status to improve handling of incomplete data.
    
    (portable-main)

--- src/main/java/com/example/ocrdesktop/utils/Receipt.java
+++ src/main/java/com/example/ocrdesktop/utils/Receipt.java
@@ -1,5 +1,7 @@
 package com.example.ocrdesktop.utils;
 
+import com.example.ocrdesktop.AppContext;
+
 import java.nio.file.Path;
 import java.sql.Timestamp;
 import java.util.HashMap;
@@ -8,29 +10,49 @@ import java.util.HashMap;
 
 public class Receipt {
     public String receiptId;
-    public String receiptTypeName;
     public String receiptTypeId;
     public String requestId;
-    public String imageUrl;
-    public ReceiptStatus status; // Can be converted to an Enum if needed
+    public String imageUrl = "";
+    public ReceiptStatus status = ReceiptStatus.PENDING; // Can be converted to an Enum if needed
     public HashMap<Integer, String> ocrData = new HashMap<>();
     public String approvedByUserId;
     public Timestamp approvedAt; // Use String for simplicity, convert to Date/Timestamp as needed
     public Path imagePath;
     // note that there are two Receipt constructor
     // these constructor for ui
-    public Receipt(String receiptId, String receiptTypeId, String requestId, String imageUrl, String status, HashMap<Integer, String> ocrData, String approvedByUserId, Timestamp approvedAt) {
+    // saving cached images in the dir to make it more efficient to retrieve the path
+    public Receipt(String receiptId, String receiptTypeId, String requestId, String imageUrl, String status,
+                   HashMap<Integer, String> ocrData, String approvedByUserId, Timestamp approvedAt, String imagePath) {
         this.receiptId = receiptId;
         this.receiptTypeId = receiptTypeId;
         this.requestId = requestId;
         this.imageUrl = imageUrl;
+
         this.status = ReceiptStatus.valueOf(status);
         if (ocrData != null) {
             this.ocrData = ocrData;
         }
         this.approvedByUserId = approvedByUserId;
         this.approvedAt = approvedAt;
+        if (imagePath != null) {
+            this.imagePath = Path.of(imagePath);
+        }else {
+            this.imagePath = Path.of(AppContext.BrokenImagePath);
+        }
     }
+
+    //in case of creating a new receipt locally, apply default values and 3 variable parameters
+    public Receipt(String receiptId, String requestId, String receiptTypeId, String imagePath) {
+        this.receiptId = receiptId;
+        this.requestId = requestId;
+        this.receiptTypeId = receiptTypeId;
+        if (imagePath != null) {
+            this.imagePath = Path.of(imagePath);
+        }else {
+            this.imagePath = Path.of(AppContext.BrokenImagePath);
+        }
+    }
+
 
     public enum ReceiptStatus {
         PENDING, // not processed by the OCR module
